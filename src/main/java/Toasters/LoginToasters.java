b66c953d1b32381f7a2dd55fe5dba63bc5d0c37c
Mahmoud <mayman@horizonssoftware.com>
2024-08-31 11:08:29 +0300

    swaglab_login_02

--- src/main/java/Toasters/LoginToasters.java
+++ src/main/java/Toasters/LoginToasters.java
@@ -3,14 +3,25 @@ package Toasters;
 import Utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.*;
+
+import java.time.Duration;
 
 public class LoginToasters {
-    private static WebDriver driver = WebDriverFactory.getDriver();
+    private static final WebDriver driver = WebDriverFactory.getDriver();
 
 
-    public static void invalidLoginToaster ()
-    {
-        driver.findElement(By.xpath(""));
+    public String invalidLoginToaster() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        //WebElement errorMsg = driver.findElement(By.cssSelector("h3[data-test=\"error]"));
+       // WebElement invalidLoginToaster = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]"));
+        // Assert.assertTrue(errorMsg.isDisplayed());
+        String invalidLogintoaster = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3/text()")).getText();
 
+        return invalidLogintoaster;
     }
 }
+
+
+
